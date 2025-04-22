23fe17ca0f71fbfec939f2b3be59b49e4d458ac7
Natareno2124 <105888353+Natareno2124@users.noreply.github.com>
2025-04-21 20:09:03 -0600

    Cambio en el menú

--- JS/nav.js
+++ JS/nav.js
@@ -1,8 +1,19 @@
-const menu= document.getElementById('menu')
-const sidebar= document.getElementById('sidebar')
+const menuItemDropDown = document.querySelectorAll('.menu-item-dropdown');
 
-menu.addEventListener('click',()=>{
-    sidebar.classList.toggle('menu-toggle')
-    menu.classList.toggle('menu-toggle')
-})
+menuItemDropDown.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+        const subMenu = menuItem.querySelector('.sub-menu');
+        const isActive = menuItem.classList.toggle('sub-menu-toggle');
+
+        if (subMenu) {
+            if (isActive) {
+                subMenu.style.height = `${subMenu.scrollHeight + 6}px`;
+                subMenu.style.padding = '0.2rem 0';
+            } else {
+                subMenu.style.height = '0';
+                subMenu.style.padding = '0';
+            }
+        }
+    });
+});
 
